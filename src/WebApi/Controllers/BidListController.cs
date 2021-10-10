11b2d2dc3301432098366cdf7997afd68008aea8
Gerald Chifanzwa <gchifanzwa@zss.co.zw>
2021-10-10 14:46:17 +0200

    Moved WebApi into WebApi folder

--- src/WebApi/Controllers/BidListController.cs
+++ src/WebApi/Controllers/BidListController.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Dot.Net.WebApi.Domain;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
- 
+
 namespace Dot.Net.WebApi.Controllers
 {
     [Route("[controller]")]
