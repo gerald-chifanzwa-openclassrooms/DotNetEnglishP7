dd831ad4440a0319ad28500974c722fca1923f4f
Gerald Chifanzwa <gchifanzwa@zss.co.zw>
2021-10-16 18:45:17 +0200

    implemented RNG cryptography to hash passwords

--- src/WebApi/Services/PasswordHashService.cs
+++ src/WebApi/Services/PasswordHashService.cs
@@ -1,10 +1,36 @@
-﻿namespace WebApi.Services
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApi.Services
 {
     public class PasswordHashService : IPasswordHashService
     {
+        private readonly IConfiguration _configuration;
+
+        public PasswordHashService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public string Hash(string plainText)
         {
-            return plainText;
+            const string vector = "A3519D704D397195";
+            var key = _configuration.GetSection("Security:EncryptionKey").Get<string>();
+            var bytes1 = Encoding.UTF8.GetBytes(vector);
+            var bytes2 = Encoding.UTF8.GetBytes(plainText);
+            var bytes3 = new Rfc2898DeriveBytes(key, bytes1).GetBytes(32);
+            var rijndaelManaged = new RijndaelManaged { Mode = CipherMode.CBC };
+            var encryptor = rijndaelManaged.CreateEncryptor(bytes3, bytes1);
+            var memoryStream = new MemoryStream();
+            var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+            cryptoStream.Write(bytes2, 0, bytes2.Length);
+            cryptoStream.FlushFinalBlock();
+            var array = memoryStream.ToArray();
+            memoryStream.Close();
+            cryptoStream.Close();
+            return Convert.ToBase64String(array);
         }
     }
 }
